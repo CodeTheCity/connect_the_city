c4107f4956ac182a1a79bbfea7d8d4bd4dc84ced
Jack Gilmore <46202639+JackGilmore@users.noreply.github.com>
2023-03-18 19:58:32 +0000

    neo4j and some stats

--- get_repositories.py
+++ get_repositories.py
@@ -7,9 +7,21 @@ g = Github(token)
 
 my_repos = g.get_organization("codethecity").get_repos()
 
+contributor_list = []
+
 for repo in my_repos:
     print(repo.name)
     contributors = repo.get_contributors()
     print("\t" + str(contributors.totalCount) + " total contributors:")
     for contributor in contributors:
-        print("\t" + contributor.login)
+        print("\t\t" + contributor.login)
+        contributor_list.append(contributor.login)
+
+
+
+distinct_list = set(contributor_list)
+
+print('=====')
+print(str(len(contributor_list)) + " instances of users contributing to a project")
+print(str(len(distinct_list)) + " distinct contributors found across " + str(my_repos.totalCount) + " total repos")
+print('=====')
